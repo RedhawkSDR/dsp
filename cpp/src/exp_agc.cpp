12e812b6144d4365f0db4e34010cc5613e508a4b
Kipp Bowen <jkb@axiosengineering.com>
2015-03-04 09:06:49 -0500

    fixed include path for exp_agc.h

--- cpp/src/exp_agc.cpp
+++ cpp/src/exp_agc.cpp
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License along with this 
  * program.  If not, see http://www.gnu.org/licenses/.
  */
-#include "../inc/exp_agc.h"
+#include "exp_agc.h"
 #include <iostream>
 #include <complex>
 
